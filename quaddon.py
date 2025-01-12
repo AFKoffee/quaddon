f1933b69c640873ec26f2332f09375d5197245b7
AFKoffee <AFKoffee@protonmail.com>
2025-01-12 20:11:17 +0100

    sample a random matrix

---
+++ quaddon.py
@@ -0,0 +1,11 @@
+## Quaddon
+import numpy as np
+
+# 1. Create a random matrix
+rows = 256
+cols = 256
+W_orig = np.random.uniform(-10.0, 10.0, (rows, cols))
+
+
+# 2. Create a transformation matrix
+
